5a32a68debd33ea7c6c46bf48e7a450f3f94aa22
VneTraffiqua <safe72again@gmail.com>
2022-05-27 04:12:54 -0700

    1 commit

--- app_converter/app_converter/settings.py
+++ app_converter/app_converter/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gza5+92fwpuj#0o+2#kppl#6(m9=2+1q%26$*6l+=5j7dj-zfk'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
